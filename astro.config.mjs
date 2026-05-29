4047ce9d68646b2a8d5fb85483b88c1d53bcd505
shotoguazi <a_guazi_520@outlook.com>
2026-05-29 13:39:36 +0800

    update favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://koicomic.com',
+  site: 'https://acgsh.com',
   trailingSlash: 'always',
   build: {
     format: 'directory',
